3db1ff9b2326ac5cc6b29872cea288bd6733f160
glmemail <glmemail@126.com>
2020-05-25 07:28:59 +0000

    Usert Message Created

---
+++ app/Admin/Controllers/MessageController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Message;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class MessageController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'App\Models\Message';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Message());
+        // 第一列显示id字段，并将这一列设置为可排序列
+        $grid->column('id', 'ID')->sortable();
+        // 第二列显示title字段，由于title字段名和Grid对象的title方法冲突，所以用Grid的column()方法代替
+        $grid->column('message', 'Message');
+        $grid->column('actiontime', 'ActionTime');
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Message::findOrFail($id));
+        $show->field('id', __('Id'));
+        $show->field('message', __('Message'));
+        $show->field('actiontime', __('ActionTime'));
+
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Message());
+        $form->text('message', __('Message'));
+        $form->datetime('actiontime', __('ActionTime'))->default(date('Y-m-d H:i:s'));
+        $timestamps = false;
+
+        return $form;
+    }
+}
---
+++ app/Models/Message.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Message;
+use Encore\Admin\Grid;
+use Encore\Admin\Facades\Admin;
+
+class Message extends Model
+{
+    // use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'message'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'actiontime' => 'datetime',
+    ];
+}
