ace09f8053a4413d576d2d19e325c4d80aa29fb6
glmemail <glmemail@126.com>
2020-05-26 05:38:54 +0000

    chart.js import

--- resources/views/admin/chartjs.blade.php
+++ resources/views/admin/chartjs.blade.php
@@ -1,4 +1,5 @@
-<script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.bundle.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.js"></script>
