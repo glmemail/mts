aa20df3b20d62d725fac4aedd603dcaa710c32d4
glmemail <glmemail@126.com>
2020-05-29 10:01:28 +0000

    css js

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,4 +13,8 @@ Route::group([
     $router->get('/', 'HomeController@index')->name('admin.home');
     $router->resource('users', UserController::class);
     $router->resource('message', MessageController::class);
+    $router->get('/chartjs', 'HomeController@index')->name('admin.home');
+
+
+
 });
