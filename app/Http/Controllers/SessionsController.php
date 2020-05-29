7bad4a1d63daa71ef9fe44b74cab0facfd0b8844
glmemail <glmemail@126.com>
2020-05-29 03:22:22 +0000

    7.2

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class SessionsController extends Controller
 {
     public function create()
     {
         return view('sessions.create');
+    }
+    public function store(Request $request)
+    {
+       $credentials = $this->validate($request, [
+           'name' => 'required|max:255',
+           'password' => 'required'
+       ]);
+       if (Auth::attempt($credentials)) {
+           session()->flash('success', '欢迎回来！');
+           return redirect()->route('users.show', [Auth::user()]);
+       } else {
+           session()->flash('danger', '很抱歉，您的用户名和密码不匹配');
+           return redirect()->back()->withInput();
+       }
+
     }
 }
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,16 +1,21 @@
+@extends('layouts.default')
+@section('title', '登录')
+
+@section('content')
 <div class="offset-md-2 col-md-8">
   <div class="card ">
     <div class="card-header">
       <h5>登录</h5>
     </div>
     <div class="card-body">
+      @include('shared._errors')
 
       <form method="POST" action="{{ route('login') }}">
           {{ csrf_field() }}
 
           <div class="form-group">
-            <label for="email">邮箱：</label>
-            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+            <label for="name">用户名：</label>
+            <input type="text" name="name" class="form-control" value="{{ old('name') }}">
           </div>
 
           <div class="form-group">
@@ -22,7 +27,9 @@
       </form>
 
       <hr>
-
+<!--
+      <p>还没账号？<a href="{{ route('signup') }}">现在注册！</a></p> -->
     </div>
   </div>
 </div>
+@stop
