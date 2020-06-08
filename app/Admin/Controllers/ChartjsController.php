d6e24d653fbd9c9a31945bd7be0aa4902e27f86b
glmemail <glmemail@126.com>
2020-06-08 11:27:57 +0000

    chatyjs OK!

--- app/Admin/Controllers/ChartjsController.php
+++ app/Admin/Controllers/ChartjsController.php
@@ -3,7 +3,7 @@
 namespace App\Admin\Controllers;
 
 
-use App\Models\action_info;
+use App\Models\Action_info;
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\Dashboard;
 use Encore\Admin\Layout\Column;
@@ -16,18 +16,33 @@ class ChartjsController extends Controller
 {
     public function index(Content $content)
     {
+//         $gender = $content->select(DB::raw('count(actiontype) as count, actiontype'))
+//             ->groupBy('actiontype')->get()->pluck('count', 'actiontype')->toArray();
+        $grid = new Grid(new Action_info());
+        // $actoninfo = DB::raw('count(actiontype) as count, actiontype');
+//             ->groupBy('actiontype')->get()->pluck('count', 'actiontype')->toArray();
+
+        // $result = DB::table("action_info")
+        // ->groupBy('actiontype')
+        // ->count();
+
+        // $g = $grid->model()->groupBy('actiontype')->get()->pluck('count', 'actiontype')->toArray();
+
+        // $g = $grid->model()->get()->groupBy('actiontype')->toArray();
+        $actoninfo = Action_info::select(array('message_id', 'actiontype'))
+            // ->where('actiontype', 'WECHAT')
+            // ->where('language', 'cn')
+            // ->orderBy('id')
+            ->get()
+            ->groupBy('actiontype')->toArray(); // 可按type分组
+
+//         $g = $content->select(DB::raw('count(actiontype) as count, actiontype'))->get()
+//             ->groupBy('actiontype')->get()->pluck('count', 'actiontype');
+
+        $action_count = [count($actoninfo["WECHAT"]), count($actoninfo["MAIL"]), count($actoninfo["PHONE"]), 0, 0, 0];
         return $content
             ->header('Chartjs')
-            ->body(new Box('Bar chart', view('admin.chartjs')));
-        $grid = new Grid(new action_info());
 
-//         $grid->header(function ($query) {
-//         $gender = $query->select(DB::raw('count(sex) as count, sex'))
-//         ->groupBy('sex')->get()->pluck('count', 'sex')->toArray();
-//         $doughnut = view('admin.chart.gender', compact('gender'));
-
-//         return new Box('性别比例', $doughnut);
-
-// });
+            ->body(new Box('Bar', view('admin.chartjs', compact('action_count'))));
     }
 }
--- resources/views/admin/chartjs.blade.php
+++ resources/views/admin/chartjs.blade.php
@@ -5,26 +5,89 @@
 <div class="col-md-4">
     <canvas id="myChart3" style="width: 322px; display: block; height: 160px;" width="322" height="160" class="chartjs-render-monitor"></canvas>
 </div>
+</div>
 <script>
 $(function () {
     var ctx = document.getElementById("myChart1").getContext('2d');
+//     var myChart = new Chart(ctx, {
+//         type: 'bar',
+//         data: {
+//             labels: ["Mail", "Phone", "Wechat", "SMS", "SSM", "Redmine"],
+//             datasets: [{
+//                 label: '# of Votes',
+// 　　　　　　　　　　//数据类型 折线图
+//                 type: "line",
+//                 // data: [12, 19, 3, 5, 2, 3],
+//                 data: [{{ $action_count[0] }}, {{ $action_count[1] }}, {{ $action_count[2] }}, 0, 0, 0],
+//                 backgroundColor:'rgba(54, 162, 235, 0.1)',
+//                 borderColor:'rgba(255,99,132,1)',
+//                 borderWidth: 1
+//             },{
+//                 label: ["Mail", "Phone", "Wechat", "SMS", "SSM", "Redmine"],
+// 　　　　　　　　　　//数据类型 柱状图
+//                 type: "bar",
+//                 data: [{{ $action_count[0] }}, {{ $action_count[1] }}, {{ $action_count[2] }}, 0, 0, 0],
+//                 backgroundColor: [
+//                     'rgba(255, 99, 132, 0.2)',
+//                     'rgba(54, 162, 235, 0.2)',
+//                     'rgba(255, 206, 86, 0.2)',
+//                     'rgba(75, 192, 192, 0.2)',
+//                     'rgba(153, 102, 255, 0.2)',
+//                     'rgba(255, 159, 64, 0.2)'
+//                 ],
+//                 borderColor: [
+//                     'red',
+//                     'rgba(54, 162, 235, 1)',
+//                     'rgba(255, 206, 86, 1)',
+//                     'rgba(75, 192, 192, 1)',
+//                     'rgba(153, 102, 255, 1)',
+//                     'rgba(255, 159, 64, 1)'
+//                 ],
+//                 borderWidth: 1
+//             }]
+//         },
+
+//         options: {
+// 　　　　　　　　//显示数值
+//             "animation": {
+//                 "duration": 1,
+//                 "onComplete": function() {
+//                     var chartInstance = this.chart,
+//                     ctx = chartInstance.ctx;
+
+//                     ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+// 　　　　　　　　　　　　//设置字体颜色
+//                     ctx.fillStyle = "black";
+//                     ctx.textAlign = 'center';
+//                     ctx.textBaseline = 'bottom';
+
+//                     this.data.datasets.forEach(function(dataset, i) {
+//                     var meta = chartInstance.controller.getDatasetMeta(i);
+//                     meta.data.forEach(function(bar, index) {
+//                         var data = dataset.data[index];
+//                         ctx.fillText(data, bar._model.x, bar._model.y - 5);
+//                     });
+//                     });
+//                 }
+//             },
+//             scales: {
+//                 yAxes: [{
+//                     ticks: {
+//                         beginAtZero:true
+//                     }
+//                 }]
+//             }
+//         }
+//     });
+
+
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
             labels: ["Mail", "Phone", "Wechat", "SMS", "SSM", "Redmine"],
             datasets: [{
                 label: '# of Votes',
-　　　　　　　　　　//数据类型 折线图
-                type: "line",
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor:'rgba(54, 162, 235, 0.1)',
-                borderColor:'rgba(255,99,132,1)',
-                borderWidth: 1
-            },{
-                label: '# of Votes',
-　　　　　　　　　　//数据类型 柱状图
-                type: "bar",
-                data: [16, 13, 11, 15, 2, 3],
+                data: [{{ $action_count[0] }}, {{ $action_count[1] }}, {{ $action_count[2] }}, 0, 0, 0],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
@@ -34,7 +97,7 @@ $(function () {
                     'rgba(255, 159, 64, 0.2)'
                 ],
                 borderColor: [
-                    'red',
+                    'rgba(255,99,132,1)',
                     'rgba(54, 162, 235, 1)',
                     'rgba(255, 206, 86, 1)',
                     'rgba(75, 192, 192, 1)',
@@ -44,30 +107,7 @@ $(function () {
                 borderWidth: 1
             }]
         },
-
         options: {
-　　　　　　　　//显示数值
-            "animation": {
-                "duration": 1,
-                "onComplete": function() {
-                    var chartInstance = this.chart,
-                    ctx = chartInstance.ctx;
-
-                    ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
-　　　　　　　　　　　　//设置字体颜色
-                    ctx.fillStyle = "black";
-                    ctx.textAlign = 'center';
-                    ctx.textBaseline = 'bottom';
-
-                    this.data.datasets.forEach(function(dataset, i) {
-                    var meta = chartInstance.controller.getDatasetMeta(i);
-                    meta.data.forEach(function(bar, index) {
-                        var data = dataset.data[index];
-                        ctx.fillText(data, bar._model.x, bar._model.y - 5);
-                    });
-                    });
-                }
-            },
             scales: {
                 yAxes: [{
                     ticks: {
@@ -78,17 +118,14 @@ $(function () {
         }
     });
 
-
-
-
-    var ctx = document.getElementById("myChart3").getContext('2d');
-    var myChart = new Chart(ctx, {
+    var ctx3 = document.getElementById("myChart3").getContext('2d');
+    var myChart3 = new Chart(ctx3, {
         type: 'doughnut',
         data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            labels: ["Mail", "Phone", "Wechat", "SMS", "SSM", "Redmine"],
             datasets: [{
                 label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
+                data: [{{ $action_count[0] }}, {{ $action_count[1] }}, {{ $action_count[2] }}, 0, 0, 0],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
