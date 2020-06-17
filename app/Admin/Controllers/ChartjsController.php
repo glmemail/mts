cc9e73e78ee74e7a05bf6554667e8276c35492dd
glmemail <glmemail@126.com>
2020-06-17 10:21:13 +0000

    click

--- app/Admin/Controllers/ChartjsController.php
+++ app/Admin/Controllers/ChartjsController.php
@@ -21,70 +21,49 @@ class ChartjsController extends Controller
         $grid = new Grid(new Action_info());
 
         // $g = $grid->model()->groupBy('actiontype')->get()->pluck('count', 'actiontype')->toArray();
-
         // $g = $grid->model()->get()->groupBy('actiontype')->toArray();
         $user = Auth::guard('admin')->user();
         // $fluentd = Fluentd::select(array('keyname'))
         //     ->get()
         //     ->groupBy('keyname');
         $fluentd = Fluentd::select(array('fluentd.keyid','fluentd.keyname','fluentd.sysid', 'fluentd.svrid', 'fluentd.subsysid', 'fluentd.cmpid','user_fluentd.user_id'))
-        // $fluentd = Fluentd::select('fluentd.keyid','fluentd.keyname','fluentd.sysid', 'fluentd.svrid', 'fluentd.subsysid', 'fluentd.cmpid','user_fluentd.user_id')
             ->join('user_fluentd','fluentd.keyid','=','user_fluentd.fluentd_keyid')
             ->where('user_fluentd.user_id', $user['username'])
-            // ->where('language', 'cn')
-            // ->orderBy('id');
             ->get()
             ->groupBy('keyid');
         // $svrid = "";
         $fluentd_json = json_decode($fluentd,true);
-        // var_dump($fluentd_json);
-        // var_dump($fluentd);
         $s = [];
         foreach ($fluentd_json as $k => $v) {
-            $s[] = $v[0]['sysid'] ." ". $v[0]['svrid'] ." ". $v[0]['subsysid'] ." ". $v[0]['subsysid'];
+            $s[] = $v[0]['sysid'] ." ". $v[0]['svrid'] ." ". $v[0]['subsysid'] ." ". $v[0]['cmpid'];
         }
         // var_dump($s);
         $fluentd_sel = $s;
-        // var_dump($fluentd_json);
-        // $count_fluentd = count($fluentd);
-        // var_dump($fluentd_json);
-        // var_dump($fluentd);
-        // foreach($fluentd_json as $key=>$value){
-        //     // echo 'key: '.$key.' --- value: '.$value.'<br/>';
-        //     var_dump($key);
-        //     var_dump($value);
-        //     $value[0];
-        //     $json = json_encode($value);
-        //     // var_dump($json);/ $json['sysid'];
-        //     // foreach($fluentd_json as $key1=>$value1){
-        //     //     var_dump($key1);
-        //     //     var_dump($value1);
-
-        //     // }
-        // }
-
-
-
-        // for ($i = 0; $i < $count_fluentd; $i++)
-        // {
-
-            // $sysid = $fluentd_json[$i]['sysid'];
-            // $svrid = $fluentd_json[$i]['svrid'];
-            // $subsysid = $fluentd_json[$i]['subsysid'];
-            // $cmpid = $fluentd_json[$i]['c'];
-            // $user_id = $fluentd_json[$i]['user_id'];
-            // $message = json_encode($de_json[$i]['data']);
-
-        // }
-
-        $actoninfo = Action_info::select(array('message_id', 'actiontype', 'message.message'))
+        $actoninfo = Action_info::select(array('action_info.message_id', 'action_info.actiontype', 'phone_info.phone_number', 'mail_info.mail_to', 'wechat_info.wechat_to'))
             ->join('message','action_info.message_id','=','message.id')
-            // ->where('actiontype', 'WECHAT')
-            // ->where('language', 'cn')
+            ->join('fluentd',function ($join) {
+                $join->on('message.sysid', '=', 'fluentd.sysid')
+                    ->on('message.svrid', '=', 'fluentd.svrid')
+                    ->on('message.subsysid', '=', 'fluentd.subsysid')
+                    ->on('message.cmpid', '=', 'fluentd.cmpid');
+                })
+            ->join('user_fluentd',function ($join) {
+                $join->on('fluentd.keyid', '=', 'user_fluentd.fluentd_keyid');
+                })
+            ->leftjoin('phone_info','action_info.phone_id','=','phone_info.call_id')
+            ->leftjoin('mail_info','action_info.mail_id','=','mail_info.id')
+            ->leftjoin('wechat_info','action_info.wechat_id','=','wechat_info.id')
+            ->where('user_fluentd.user_id', $user['username'])
             // ->orderBy('id')
             ->get()
             ->groupBy('actiontype'); // 按actiontype分组
         $actoninfo_json = json_decode($actoninfo,TRUE);
+        var_dump($actoninfo_json);
+
+        foreach ($actoninfo as $k1 => $v1) {
+        }
+
+
         // $count_json = count($actoninfo_json);
         // for ($i = 0; $i < $count_json; $i++)
         // {
@@ -98,39 +77,11 @@ class ChartjsController extends Controller
         //     // $message = json_encode($de_json[$i]['data']);
         // }
 
-        $str = "";
-        // $str = $str.'start【'.$fluentd;
         $cnt = 0;
-        // $g[0];
-        // foreach ($g as $value)
-        // {
-            // if($cnt == 0) {
-            //     $str = $value->count;
-            // }
-            // else{
-            //     $str = $str.','.$value->count;
-            // }
-            // $cnt++;
-        // }
-        // $str = $str.'】end';
         $action_count  = [count($actoninfo["WECHAT"]), count($actoninfo["MAIL"]), count($actoninfo["PHONE"]), 0, 0, 0];
         return $content
             ->header('Chartjs')
-
-            ->body(new Box('Bar'.':'.$str, view('admin.chartjs', compact('action_count'), compact('fluentd_sel'))));
-
-    //     $grid->header(function ($content) {
-
-    //         $gender = $content->select(DB::raw('count(actiontype) as count, actiontype'))
-
-    //         ->groupBy('actiontype')->get()->pluck('count', 'actiontype')->toArray();
-
-    //         $doughnut = view('admin.chartjs', compact('gender'));
-
-    //         return new Box('性别比例', $doughnut);
-
-    //     });
-
+            ->body(new Box('', view('admin.chartjs', compact('action_count'), compact('fluentd_sel'), compact('actoninfo_json'))));
 
     }
 
--- resources/views/admin/chartjs.blade.php
+++ resources/views/admin/chartjs.blade.php
@@ -7,7 +7,7 @@
         foreach ($fluentd_sel as $v) {
             echo "<option value='".$v."''>".$v."</option>";
         }
-    ?>
+        ?>
     </select>
 </div>
 <div class="row">
@@ -21,78 +21,6 @@
 <script>
 $(function () {
     var ctx = document.getElementById("myChart1").getContext('2d');
-//     var myChart = new Chart(ctx, {
-//         type: 'bar',
-//         data: {
-//             labels: ["Mail", "Phone", "Wechat", "SMS", "SSM", "Redmine"],
-//             datasets: [{
-//                 label: '# of Votes',
-// 　　　　　　　　　　//数据类型 折线图
-//                 type: "line",
-//                 // data: [12, 19, 3, 5, 2, 3],
-//                 data: [{{ $action_count[0] }}, {{ $action_count[1] }}, {{ $action_count[2] }}, 0, 0, 0],
-//                 backgroundColor:'rgba(54, 162, 235, 0.1)',
-//                 borderColor:'rgba(255,99,132,1)',
-//                 borderWidth: 1
-//             },{
-//                 label: ["Mail", "Phone", "Wechat", "SMS", "SSM", "Redmine"],
-// 　　　　　　　　　　//数据类型 柱状图
-//                 type: "bar",
-//                 data: [{{ $action_count[0] }}, {{ $action_count[1] }}, {{ $action_count[2] }}, 0, 0, 0],
-//                 backgroundColor: [
-//                     'rgba(255, 99, 132, 0.2)',
-//                     'rgba(54, 162, 235, 0.2)',
-//                     'rgba(255, 206, 86, 0.2)',
-//                     'rgba(75, 192, 192, 0.2)',
-//                     'rgba(153, 102, 255, 0.2)',
-//                     'rgba(255, 159, 64, 0.2)'
-//                 ],
-//                 borderColor: [
-//                     'red',
-//                     'rgba(54, 162, 235, 1)',
-//                     'rgba(255, 206, 86, 1)',
-//                     'rgba(75, 192, 192, 1)',
-//                     'rgba(153, 102, 255, 1)',
-//                     'rgba(255, 159, 64, 1)'
-//                 ],
-//                 borderWidth: 1
-//             }]
-//         },
-
-//         options: {
-// 　　　　　　　　//显示数值
-//             "animation": {
-//                 "duration": 1,
-//                 "onComplete": function() {
-//                     var chartInstance = this.chart,
-//                     ctx = chartInstance.ctx;
-
-//                     ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
-// 　　　　　　　　　　　　//设置字体颜色
-//                     ctx.fillStyle = "black";
-//                     ctx.textAlign = 'center';
-//                     ctx.textBaseline = 'bottom';
-
-//                     this.data.datasets.forEach(function(dataset, i) {
-//                     var meta = chartInstance.controller.getDatasetMeta(i);
-//                     meta.data.forEach(function(bar, index) {
-//                         var data = dataset.data[index];
-//                         ctx.fillText(data, bar._model.x, bar._model.y - 5);
-//                     });
-//                     });
-//                 }
-//             },
-//             scales: {
-//                 yAxes: [{
-//                     ticks: {
-//                         beginAtZero:true
-//                     }
-//                 }]
-//             }
-//         }
-//     });
-
-
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
@@ -164,12 +92,16 @@ $(function () {
                         beginAtZero:true
                     }
                 }]
-            }
+            },
+            events : ["mousemove", "mouseout", "click"],
+            onClick : function (event, bars){
+                             var activeElement = bars[0];   //当前被选中的元素
+                             var product = activeElement._model.label;
+                             console.log(activeElement);
+                             alert(activeElement);
+                             //load_version_chart(product);
+                         }
         }
     });
 });
-
-    // function randomScalingFactor() {
-    //     return Math.floor(Math.random() * 100)
-    // }
 </script>
